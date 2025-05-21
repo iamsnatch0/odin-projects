1c9b9b2aef5ad6808e854bccd181c047a4fceb6f
Milvin <44063014+iamsnatch0@users.noreply.github.com>
2025-05-21 23:33:57 +0100

    Created Divs

--- project_etch_a_sketch/sketch.js
+++ project_etch_a_sketch/sketch.js
@@ -1,11 +1,39 @@
 // Create the divs using JavaScript. Don’t try to create them by hand by 
 // copying and pasting them in your HTML file!
-const body = document.querySelector("body");
+const body = document.body;
+const divC = document.createElement("div");
+divC.style.height = "500px";
+divC.style.borderStyle = "solid";
+divC.setAttribute("id", "container");
 
-const newDiv = document.createElement("div");
-newDiv.style.height = "500px";
-newDiv.style.borderStyle = "solid";
-newDiv.getAttribute("id");
+document.querySelector("div");
+const board = document.createElement("div");
+divC.append(board);
+board.setAttribute("class", "board");
+
+
+const divChange = document.createElement("div");
+divChange.setAttribute("class", "changes");
+
+document.querySelector(".changes");
+const colorChange = document.createElement("div");
+colorChange.innerText = "Change Color"
+divChange.append(colorChange);
+colorChange.setAttribute("class", "colorChange");
+
+
+document.querySelector(".changes");
+const gridChange = document.createElement("div");
+gridChange.innerText = "Grid Change"
+divChange.append(gridChange);
+gridChange.setAttribute("class", "gridChange");
+
+
+const footer = document.createElement("footer");
+footer.innerText = "© 2025 - Present | Iamsnatch0";
+
+
+body.append(divC,divChange, footer);
 
 
 
