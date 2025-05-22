f5177a72869e3126787110a424ecdb23f60e5a5c
Milvin <44063014+iamsnatch0@users.noreply.github.com>
2025-05-22 21:50:12 +0100

    Error Made, Create With HTMl

--- project_etch_a_sketch/sketch.js
+++ project_etch_a_sketch/sketch.js
@@ -1,39 +1,42 @@
 // Create the divs using JavaScript. Don’t try to create them by hand by 
 // copying and pasting them in your HTML file!
-const body = document.body;
-const divC = document.createElement("div");
-divC.style.height = "500px";
-divC.style.borderStyle = "solid";
-divC.setAttribute("id", "container");
+// const body = document.body;
+// const divC = document.createElement("div");
+// divC.style.height = "500px";
+// divC.style.borderStyle = "solid";
+// divC.setAttribute("id", "container");
 
-document.querySelector("div");
-const board = document.createElement("div");
-divC.append(board);
-board.setAttribute("class", "board");
+// document.querySelector("div");
+// const board = document.createElement("div");
+// divC.append(board);
+// board.setAttribute("class", "board");
 
-
-const divChange = document.createElement("div");
-divChange.setAttribute("class", "changes");
-
-document.querySelector(".changes");
-const colorChange = document.createElement("div");
-colorChange.innerText = "Change Color"
-divChange.append(colorChange);
-colorChange.setAttribute("class", "colorChange");
+// document.querySelector("body");
 
 
-document.querySelector(".changes");
-const gridChange = document.createElement("div");
-gridChange.innerText = "Grid Change"
-divChange.append(gridChange);
-gridChange.setAttribute("class", "gridChange");
+
+// const divChange = document.createElement("div");
+// divChange.setAttribute("class", "changes");
+
+// document.querySelector(".changes");
+// const colorChange = document.createElement("div");
+// colorChange.innerText = "Change Color"
+// divChange.append(colorChange);
+// colorChange.setAttribute("class", "colorChange");
 
 
-const footer = document.createElement("footer");
-footer.innerText = "© 2025 - Present | Iamsnatch0";
+// document.querySelector(".changes");
+// const gridChange = document.createElement("div");
+// gridChange.innerText = "Grid Change"
+// divChange.append(gridChange);
+// gridChange.setAttribute("class", "gridChange");
 
 
-body.append(divC,divChange, footer);
+// const footer = document.createElement("footer");
+// footer.innerText = "© 2025 - Present | Iamsnatch0";
+
+
+// body.append(divC,divChange, footer);
 
 
 
