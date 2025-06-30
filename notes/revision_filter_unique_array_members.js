b621b1b3c955d3985ab547ac1d15c99a4d9ebc29
Milvin <44063014+iamsnatch0@users.noreply.github.com>
2025-06-30 23:43:13 +0100

    Revision exercise Completed

---
+++ notes/revision_filter_unique_array_members.js
@@ -0,0 +1,23 @@
+// Filter unique array members
+// Let arr be an array.
+// Create a function unique(arr) that should return an array with unique items of arr.
+
+
+
+function unique(arr) {
+  let result = [];
+
+  for (let str of arr) {
+    if (!result.includes(str)) {
+      result.push(str);
+    }
+  }
+
+  return result;
+}
+
+let strings = ["Hare", "Krishna", "Hare", "Krishna",
+  "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+alert( unique(strings) ); // Hare, Krishna, :-O
