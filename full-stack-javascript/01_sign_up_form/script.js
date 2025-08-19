c9175a3585eb4c198277868b6adbda54eb66a361
Milvin <44063014+iamsnatch0@users.noreply.github.com>
2025-08-19 23:33:48 +0100

    Js Completed

---
+++ full-stack-javascript/01_sign_up_form/script.js
@@ -0,0 +1,22 @@
+document.querySelector('form').addEventListener('submit', function (e) {
+e.preventDefault(); 
+
+const name = document.getElementById('name').value;
+const email = document.getElementById('email').value;
+const password = document.getElementById('password').value;
+
+if (name === '' || email === '' || password === '') {
+alert('Please fill out all fields.');
+return;
+}
+
+const message = document.createElement('p');
+message.textContent = 'Submit done';
+message.style.color = 'green';
+message.style.fontWeight = 'bold';
+
+if (!document.querySelector('.submit-message')) {
+message.classList.add('submit-message');
+document.querySelector('.form-side').appendChild(message);
+}
+});
